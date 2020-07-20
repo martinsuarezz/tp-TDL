78c7c8fd17910c9cee8061a7881a4183cf0e677c
ignaciojaneiro <39175112+ignaciojaneiro@users.noreply.github.com>
2020-07-20 16:12:04 -0300

    Video Recommendation 20/7/2020

--- VideosRecommendation.R
+++ VideosRecommendation.R
@@ -1,191 +1,116 @@
+#Importamos libreria a usar
 library(recommenderlab)
-library(ggplot2)                       
-library(data.table)
-library(reshape2)
 
-setwd("/Users/Nacho/Desktop/movie_data")                      
-movie_data <- read.csv("movies.csv",stringsAsFactors=FALSE)
-rating_data <- read.csv("ratings.csv")
-str(movie_data)
-
-newUserID <- tail(rating_data$userId,1) + 1
-
-contador <- 0
-
-while (contador < 10) {
-  randomMovieId <- sample(1:10329,1)
-  print(movie_data$title[randomMovieId])
-  my.movieWatched <- readline(prompt="Viste la pelicula ? [1: Si, 2: No]: ")
-  my.movieWatched <- as.integer(my.movieWatched)
-  if(my.movieWatched == 1){
-    contador = contador + 1
-    my.ratingMovie <- readline(prompt="Puntea la pelicula 1-5: ")
-    my.ratingMovie <- as.integer(my.ratingMovie)
-    rating_data <- rbind(rating_data, list(newUserID, randomMovieId, my.ratingMovie, 0))
-  }
+#----------Funcion main------------------------------------------------
+main <- function(){
+  setwd("/users/Nacho/Desktop/facultad/movie_data")
+  lecturaArchivos("movies.csv","ratings.csv")
+  #pidoInput()
+  programa()
 }
 
-recomendation <- function(newUser){
-            movie_genre <- as.data.frame(movie_data$genres, stringsAsFactors=FALSE)
-            library(data.table)
-            movie_genre2 <- as.data.frame(tstrsplit(movie_genre[,1], '[|]', 
-                                                    type.convert=TRUE), 
-                                          stringsAsFactors=FALSE) 
-            colnames(movie_genre2) <- c(1:10)
-            list_genre <- c("Action", "Adventure", "Animation", "Children", 
-                            "Comedy", "Crime","Documentary", "Drama", "Fantasy",
-                            "Film-Noir", "Horror", "Musical", "Mystery","Romance",
-                            "Sci-Fi", "Thriller", "War", "Western")
-            genre_mat1 <- matrix(0,10330,18)
-            genre_mat1[1,] <- list_genre
-            colnames(genre_mat1) <- list_genre
-            for (index in 1:nrow(movie_genre2)) {
-              for (col in 1:ncol(movie_genre2)) {
-                gen_col = which(genre_mat1[1,] == movie_genre2[index,col]) #Author DataFlair
-                genre_mat1[index+1,gen_col] <- 1
-              }
-            }
-            genre_mat2 <- as.data.frame(genre_mat1[-1,], stringsAsFactors=FALSE) #remove first row, which was the genre list
-            for (col in 1:ncol(genre_mat2)) {
-              genre_mat2[,col] <- as.integer(genre_mat2[,col]) #convert from characters to integers
-            } 
-            str(genre_mat2)
-            
-            
-            SearchMatrix <- cbind(movie_data[,1:2], genre_mat2[])
-            head(SearchMatrix)
-            
-            ratingMatrix <- dcast(rating_data, userId~movieId, value.var = "rating", na.rm=FALSE)
-            ratingMatrix <- as.matrix(ratingMatrix[,-1]) #remove userIds
-            #Convert rating matrix into a recommenderlab sparse matrix
-            ratingMatrix <- as(ratingMatrix, "realRatingMatrix")
-            ratingMatrix
-            
-            recommendation_model <- recommenderRegistry$get_entries(dataType = "realRatingMatrix")
-            names(recommendation_model)
-            
-            lapply(recommendation_model, "[[", "description")
-            
-            recommendation_model$IBCF_realRatingMatrix$parameters
-            
-            similarity_mat <- similarity(ratingMatrix[1:4, ],
-                                           method = "cosine",
-                                           which = "users")
-            as.matrix(similarity_mat)
-            image(as.matrix(similarity_mat), main = "User's Similarities")
-            
-            movie_similarity <- similarity(ratingMatrix[, 1:4], method =
-                                             "cosine", which = "items")
-            as.matrix(movie_similarity)
-            image(as.matrix(movie_similarity), main = "Movies similarity")
-            
-            rating_values <- as.vector(ratingMatrix@data)
-            unique(rating_values) # extracting unique ratings
-            
-            Table_of_Ratings <- table(rating_values) # creating a count of movie ratings
-            Table_of_Ratings
-            
-            #Most Viewed Movies Visualization
-            
-            library(ggplot2)
-            movie_views <- colCounts(ratingMatrix) # count views for each movie
-            table_views <- data.frame(movie = names(movie_views),
-                                      views = movie_views) # create dataframe of views
-            table_views <- table_views[order(table_views$views,
-                                             decreasing = TRUE), ] # sort by number of views
-            table_views$title <- NA
-            for (index in 1:10325){
-              table_views[index,3] <- as.character(subset(movie_data,
-                                                          movie_data$movieId == table_views[index,1])$title)
-            }
-            table_views[1:6,]
-            
-            ggplot(table_views[1:6, ], aes(x = title, y = views)) +
-              geom_bar(stat="identity", fill = 'steelblue') +
-              geom_text(aes(label=views), vjust=-0.3, size=3.5) +
-              theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-              ggtitle("Total Views of the Top Films")
-            
-            #Heatmap of Movie Ratings
-            image(ratingMatrix[1:20, 1:25], axes = FALSE, main = "Heatmap of the first 25 rows and 25 columns")
-            
-            #Performing Data Preparation
-            movie_ratings <- ratingMatrix[rowCounts(ratingMatrix) > 50,
-                                          colCounts(ratingMatrix) > 50]
-            Movie_ratings
-            
-            minimum_movies<- quantile(rowCounts(movie_ratings), 0.98)
-            minimum_users <- quantile(colCounts(movie_ratings), 0.98)
-            image(movie_ratings[rowCounts(movie_ratings) > minimum_movies,
-                                colCounts(movie_ratings) > minimum_users],
-                  main = "Heatmap of the top users and movies")
-            
-            average_ratings <- rowMeans(movie_ratings)
-            qplot(average_ratings, fill=I("steelblue"), col=I("red")) +
-              ggtitle("Distribution of the average rating per user")
-            
-            #Data Normalization
-            
-            normalized_ratings <- normalize(movie_ratings)
-            sum(rowMeans(normalized_ratings) > 0.00001)
-            image(normalized_ratings[rowCounts(normalized_ratings) > minimum_movies,
-                                     colCounts(normalized_ratings) > minimum_users],
-            main = "Normalized Ratings of the Top Users")
-            
-            #Performing Data Binarization
-            
-            binary_minimum_movies <- quantile(rowCounts(movie_ratings), 0.95)
-            binary_minimum_users <- quantile(colCounts(movie_ratings), 0.95)
-            #movies_watched <- binarize(movie_ratings, minRating = 1)
-            good_rated_films <- binarize(movie_ratings, minRating = 3)
-            image(good_rated_films[rowCounts(movie_ratings) > binary_minimum_movies,
-                                   colCounts(movie_ratings) > binary_minimum_users],
-            main = "Heatmap of the top users and movies")
-            
-            #Collaborative Filtering System
-            sampled_data<- sample(x = c(TRUE, FALSE),
-                                  size = nrow(movie_ratings),
-                                  replace = TRUE,
-                                  prob = c(0.8, 0.2))
-            training_data <- movie_ratings[sampled_data, ]
-            testing_data <- movie_ratings[!sampled_data, ]
-            
-            #Building the Recommendation System using R
-            
-            recommendation_system <- recommenderRegistry$get_entries(dataType ="realRatingMatrix")
-            recommendation_system$IBCF_realRatingMatrix$parameters
-            
-            recommen_model <- Recommender(data = training_data,
-                                          method = "IBCF",
-                                          parameter = list(k = 30))
-            recommen_model
-            class(recommen_model)
-            
-            model_info <- getModel(recommen_model)
-            class(model_info$sim)
-            dim(model_info$sim)
-            top_items <- 20
-            image(model_info$sim[1:top_items, 1:top_items],
-                  main = "Heatmap of the first rows and columns")
-            
-            sum_rows <- rowSums(model_info$sim > 0)
-            table(sum_rows)
-            sum_cols <- colSums(model_info$sim > 0)
-            qplot(sum_cols, fill=I("steelblue"), col=I("red"))+ ggtitle("Distribution of the column count")
-            
-            #How to build Recommender System on dataset using R?
-            top_recommendations <- 10 # the number of items to recommend to each user
-            predicted_recommendations <- predict(object = recommen_model,
-                                                 newdata = testing_data,
-                                                 n = top_recommendations)
-            predicted_recommendations
-            
-            user1 <- predicted_recommendations@items[[newUser]] #Usuario ID a recomendar
-            movies_user1 <- predicted_recommendations@itemLabels[user1]
-            movies_user2 <- movies_user1
-            for (index in 1:10){
-              movies_user2[index] <- as.character(subset(movie_data,
-                                                         movie_data$movieId == movies_user1[index])$title)
-            }
-            movies_user2 #peliculas que devuelve
-}            
+main()
+#----------------------------------------------------------------------
+
+#----------Funcion lectura archivos------------------------------------
+lecturaArchivos <- function(movieFile,ratingFile){
+  # Leo los archivos
+  movie_data <- read.csv(movieFile,stringsAsFactors=FALSE)
+  rating_data <- read.csv(ratingFile)
+}
+
+#----------Funcion pide input y rating de 10 peliculas al usuario.-----
+pidoInput <- function(userID){
+  #Obtengo el rating de 10 peliculas del usuario.
+  newUserID <- tail(rating_data$userId,1) + 1
+  
+  contador <- 0
+  
+  while (contador < 10) {
+    randomMovieId <- sample(1:10329,1)
+    print(movie_data$title[randomMovieId])
+    my.movieWatched <- readline(prompt="Viste la pelicula ? [1: Si, 2: No]: ")
+    my.movieWatched <- suppressWarnings(as.integer(my.movieWatched))
+    if(my.movieWatched == 1){
+      contador = contador + 1
+      my.ratingMovie <- readline(prompt="Puntea la pelicula 1-5: ")
+      my.ratingMovie <- as.integer(my.ratingMovie)
+      rating_data <- rbind(rating_data, list(newUserID, randomMovieId, my.ratingMovie, 0))
+    }
+  }
+  
+}
+
+#----------Funciones que procesa informacion y recomienda las peliculas
+programa <- function(){
+  
+  #Proceso informacion de los archivos.
+  procesadoInformacion()
+  
+  #Creamos nuestro Sistema de filtrado colaborativo.
+  sampled_data<- sample(x = c(TRUE, FALSE),size = nrow(movie_ratings),replace = TRUE,prob = c(0.8, 0.2))
+  training_data <- movie_ratings[sampled_data, ]
+  testing_data <- movie_ratings[!sampled_data, ]
+  
+  
+  #Obtiene las 30 peliculas mas parecidas.
+  recommendation_model <- Recommender(data = training_data,method = "IBCF",parameter = list(k = 30))
+  
+  #Cantidad de peliculas a recomendar
+  top_recommendations <- 10
+  
+  #Recomendaciones para el usuario elegido.
+  predicted_recommendations <- predict(object = recommendation_model,newdata = testing_data,n = top_recommendations)
+  userSelected <- predicted_recommendations@items[[tail(rating_data$userId,1)]]
+  moviesUserSelected <- predicted_recommendations@itemLabels[userSelected]
+  moviesUserSelectedAux <- moviesUserSelected
+  
+  for (index in 1:10){
+    moviesUserSelectedAux[index] <- as.character(subset(movie_data,movie_data$movieId == moviesUserSelected[index])$title)
+  }
+  
+  print(moviesUserSelectedAux)
+}
+
+procesadoInformacion <- function(){
+  #Proceso la informacion de los archivos.
+  #Genero una matriz en donde muestra los genereos y las peliculas que tienen el determinado genero.
+  movie_genre <- as.data.frame(movie_data$genres, stringsAsFactors=FALSE)
+  movie_genre2 <- as.data.frame(tstrsplit(movie_genre[,1], "[|]"), type.convert=TRUE)
+  colnames(movie_genre2) <- c(1:10)
+  
+  list_genre <- c("Action", "Adventure", "Animation", "Children", 
+                  "Comedy", "Crime","Documentary", "Drama", "Fantasy",
+                  "Film-Noir", "Horror", "Musical", "Mystery","Romance",
+                  "Sci-Fi", "Thriller", "War", "Western")
+  
+  genre_matrix <- matrix(0,10330,18)
+  genre_matrix[1,] <- list_genre
+  colnames(genre_matrix) <- list_genre
+  
+  for (index in 1:nrow(movie_genre2)) {
+    for (col in 1:ncol(movie_genre2)) {
+      gen_col = which(genre_matrix[1,] == movie_genre2[index,col]) 
+      genre_matrix[index+1,gen_col] <- 1
+    }
+  }
+  
+  genre_matrix2 <- as.data.frame(genre_matrix[-1,], stringsAsFactors=FALSE) 
+  for (col in 1:ncol(genre_matrix2)) {
+    genre_matrix2[,col] <- as.integer(genre_matrix2[,col])
+  } 
+  
+  #Transformamos los rating a matriz
+  ratingMatrix <- dcast(rating_data,userId~movieId, value.var = "rating", na.rm=FALSE)
+  
+  #Elimino el userId
+  ratingMatrix <- as.matrix(ratingMatrix[,-1])
+  
+  # Transformo la matriz para poder usarla con recommenderlab
+  ratingMatrix <- as(ratingMatrix, "realRatingMatrix")
+  
+  movie_ratings <- ratingMatrix[rowCounts(ratingMatrix) > 50,colCounts(ratingMatrix) > 50]
+  
+}
+
+#----------------------------------------------------------------------
